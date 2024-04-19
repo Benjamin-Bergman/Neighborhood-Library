b0a9f064ed0a82f4f2822459a81ea70d4ae977a0
Benjamin-Bergman <git@gdane.net>
2024-04-19 14:14:55 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/Book.java
@@ -0,0 +1,6 @@
+// Copyright (c) Benjamin Bergman 2024.
+
+package com.pluralsight;
+
+public class Book {
+}
---
+++ src/main/java/com/pluralsight/LibraryUI.java
@@ -0,0 +1,20 @@
+// Copyright (c) Benjamin Bergman 2024.
+
+package com.pluralsight;
+
+/**
+ * Represents a library user interface.
+ */
+public class LibraryUI {
+    /**
+     * Entry point to run a library interface.
+     *
+     * @param args Ignored.
+     */
+    public static void main(String[] args) {
+        new LibraryUI().runInterface();
+    }
+
+    private void runInterface() {
+    }
+}
