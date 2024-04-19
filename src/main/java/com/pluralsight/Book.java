e12d537308b15bfd3e85a5d1bbd2f24a898320b2
Benjamin-Bergman <git@gdane.net>
2024-04-19 17:14:30 -0400

    Add remaining GUI

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -2,5 +2,66 @@
 
 package com.pluralsight;
 
-public class Book {
+@SuppressWarnings("NewClassNamingConvention")
+class Book {
+    @SuppressWarnings("FieldNamingConvention")
+    private static int idCounter = 1;
+    @SuppressWarnings("FieldNamingConvention")
+    private final String isbn;
+    private final String title;
+    @SuppressWarnings("FieldNamingConvention")
+    private final int id;
+    private boolean checkedOut;
+    private String checkedOutTo = "";
+
+    @SuppressWarnings("ParameterHidesMemberVariable")
+    Book(String isbn, String title) {
+        this.isbn = isbn;
+        this.title = title;
+        checkedOut = false;
+        id = idCounter;
+        //noinspection AssignmentToStaticFieldFromInstanceMethod
+        idCounter++;
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    int getId() {
+        return id;
+    }
+
+    String getIsbn() {
+        return isbn;
+    }
+
+    String getTitle() {
+        return title;
+    }
+
+    boolean isCheckedOut() {
+        return checkedOut;
+    }
+
+    String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    void checkout(String name) {
+        checkedOut = true;
+        checkedOutTo = name;
+    }
+
+    void checkin() {
+        checkedOut = false;
+        checkedOutTo = "";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (obj instanceof Book other) && (getId() == other.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getId();
+    }
 }
--- src/main/java/com/pluralsight/LibraryUI.java
+++ src/main/java/com/pluralsight/LibraryUI.java
@@ -4,15 +4,30 @@ package com.pluralsight;
 
 import com.googlecode.lanterna.gui2.*;
 import com.googlecode.lanterna.gui2.LinearLayout.*;
+import com.googlecode.lanterna.gui2.dialogs.*;
 import com.googlecode.lanterna.terminal.*;
 
 import java.io.*;
 import java.util.*;
+import java.util.function.*;
 
-/**
- * Represents a library user interface.
- */
-public final class LibraryUI extends BasicWindow {
+final class LibraryUI extends BasicWindow {
+    @SuppressWarnings({"SpellCheckingInspection", "HardcodedFileSeparator"})
+    private final Book[] books = {
+            new Book("1853260088", "Moby Dick"),
+            new Book("0593084691", "Humble Pi: When Math Goes Wrong in the Real World"),
+            new Book("1593272812", "Land of Lisp: Learn to Program in Lisp, One Game at a Time!"),
+            new Book("0310450470", "NIV Bible"),
+            new Book("142261039X", "Artscroll English Tanach"),
+            new Book("097730096X", "The Clear Quran"),
+            new Book("133813437X", "The Silver Eyes: Five Nights at Freddy’s"),
+            new Book("1568811306", "Winning Ways for Your Mathematical Plays: Volume 1"),
+            new Book("0743477111", "Romeo and Juliet"),
+            new Book("0765382032", "The Three-Body Problem"),
+            new Book("0762499869", "Math Games with Bad Drawings: 75 1/4 Simple, Challenging, Go-Anywhere Games―And Why They Matter"),
+            new Book("0525537090", "How To: Absurd Scientific Advice for Common Real-World Problems")
+    };
+
     private LibraryUI() {
         super("Neighborhood Library");
 
@@ -22,18 +37,12 @@ public final class LibraryUI extends BasicWindow {
         panel.setLayoutManager(new LinearLayout(Direction.VERTICAL));
 
         var justify = LinearLayout.createLayoutData(Alignment.Fill, GrowPolicy.CanGrow);
-        panel.addComponent(new Button("Show available books").setLayoutData(justify));
-        panel.addComponent(new Button("Show checked-out books").setLayoutData(justify));
+        panel.addComponent(new Button("Check out a book", this::showAvailable).setLayoutData(justify));
+        panel.addComponent(new Button("Return a book", this::showCheckedOut).setLayoutData(justify));
         panel.addComponent(new Button("Exit", this::close).setLayoutData(justify));
         setComponent(panel);
     }
 
-    /**
-     * Entry point to run a library interface.
-     *
-     * @param args Ignored.
-     * @throws IOException On an IO error.
-     */
     public static void main(String[] args) throws IOException {
         try (var screen = new DefaultTerminalFactory().createScreen()) {
             screen.startScreen();
@@ -45,5 +54,70 @@ public final class LibraryUI extends BasicWindow {
 
             screen.stopScreen();
         }
+    }
+
+    @SuppressWarnings("FeatureEnvy")
+    private void showAvailable() {
+        showBookList(
+                "Check out a book",
+                bk -> !bk.isCheckedOut(),
+                bk -> "%s - %s".formatted(bk.getIsbn(), bk.getTitle()),
+                this::checkoutBook);
+    }
+
+    @SuppressWarnings("FeatureEnvy")
+    private void showCheckedOut() {
+        showBookList(
+                "Check in a book",
+                Book::isCheckedOut,
+                bk -> "[%s] %s - %s".formatted(bk.getCheckedOutTo(), bk.getIsbn(), bk.getTitle()),
+                this::checkinBook);
+    }
+
+    private void showBookList(String title, Predicate<? super Book> filter, Function<? super Book, String> display, Consumer<? super Book> function) {
+        var builder = new ActionListDialogBuilder()
+                .setTitle(title)
+                .setDescription("Choose a book");
+
+        var usedBooks = Arrays.stream(books).filter(filter).toList();
+
+        for (var book : usedBooks)
+            //noinspection ObjectAllocationInLoop
+            builder.addAction(display.apply(book), () -> function.accept(book));
+
+        builder.build().showDialog(getTextGUI());
+    }
+
+    private void checkoutBook(Book bk) {
+        String name = TextInputDialog.showDialog(
+                getTextGUI(),
+                "Name",
+                "Please enter you name for our records.",
+                "");
+
+        if ((name == null) || name.isEmpty() || name.isBlank()) {
+            MessageDialog.showMessageDialog(
+                    getTextGUI(),
+                    "Canceled",
+                    "The book has not been checked out.",
+                    MessageDialogButton.OK);
+            return;
+        }
+
+        bk.checkout(name);
+        MessageDialog.showMessageDialog(
+                getTextGUI(),
+                "Success",
+                "Thank you for checking out %s.".formatted(bk.getTitle()),
+                MessageDialogButton.OK);
+    }
+
+    private void checkinBook(Book bk) {
+        bk.checkin();
+        MessageDialog.showMessageDialog(
+                getTextGUI(),
+                "Success",
+                "Thank you for returning %s.".formatted(bk.getTitle()),
+                MessageDialogButton.OK);
     }
 }
