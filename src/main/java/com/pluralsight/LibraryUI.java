aca74cd7d83c39b880b4378ae8f59267cbb4cb29
Benjamin-Bergman <git@gdane.net>
2024-04-19 15:06:02 -0400

    Working build system

--- src/main/java/com/pluralsight/LibraryUI.java
+++ src/main/java/com/pluralsight/LibraryUI.java
@@ -2,6 +2,10 @@
 
 package com.pluralsight;
 
+import com.googlecode.lanterna.terminal.*;
+
+import java.io.*;
+
 /**
  * Represents a library user interface.
  */
@@ -10,11 +14,19 @@ public class LibraryUI {
      * Entry point to run a library interface.
      *
      * @param args Ignored.
+     * @throws IOException If terminal creation fails.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new LibraryUI().runInterface();
     }
 
-    private void runInterface() {
+    private void runInterface() throws IOException {
+        TerminalFactory terminalFactory = new DefaultTerminalFactory();
+        try (var term = terminalFactory.createTerminal()) {
+            term.putString("Howdy");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
     }
 }
